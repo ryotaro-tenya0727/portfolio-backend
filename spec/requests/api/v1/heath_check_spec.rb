0b23278780b278e0efcb0ecf8ca196dea6a3ac5e
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-04-22 07:07:48 +0900

    Add: ユーザーを作成する仮機能実装

--- spec/requests/api/v1/heath_check_spec.rb
+++ spec/requests/api/v1/heath_check_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::HeathChecks", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/api/v1/heath_check/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+  # describe "GET /index" do
+  #   it "returns http success" do
+  #     get "/api/v1/heath_check/index"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 end
