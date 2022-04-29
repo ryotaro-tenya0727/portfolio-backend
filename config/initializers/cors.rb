3e3ba07a86f631b25b736631063421827992a7a4
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-04-29 16:20:15 +0900

    Fix: cors編集

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'http://localhost:3001', 'https://d16ambt2qp8kue.cloudfront.net'
 
     resource '*',
       headers: :any,
