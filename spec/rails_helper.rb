5316cfdc6cd1c2ea0563a88939a893810d9de71d
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-05-03 20:46:18 +0900

    Add: itをfitにして特定のテストを実行できる設定を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,7 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryBot::Syntax::Methods
+  config.filter_run_when_matching :focus
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
