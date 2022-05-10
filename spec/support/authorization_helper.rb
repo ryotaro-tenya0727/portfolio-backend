8010c785e97739160323d5e04590e055947d6c3d
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-05-10 10:40:35 +0900

    Fix: 認証のrspecのスタブの返却値をcurrent_userに変更

--- spec/support/authorization_helper.rb
+++ spec/support/authorization_helper.rb
@@ -1,11 +1,6 @@
 module AuthorizationHelper
   def registration_stub
-    @user_info = [
-      { 'sub' => 'test_sub',
-        'name' => 'test_name',
-        'role' => 'admin' }
-    ]
-    allow_any_instance_of(Api::V1::UsersController).to receive(:register_user).and_return(@user_info)
+    allow_any_instance_of(Api::V1::UsersController).to receive(:register_user).and_return(current_user)
   end
 
   def registration_exception_stub
@@ -13,6 +8,6 @@ module AuthorizationHelper
   end
 
   def authorization_stub
-    allow_any_instance_of(SecuredController).to receive(:authorize_request).and_return(@user_info)
+    allow_any_instance_of(SecuredController).to receive(:authorize_request).and_return(current_user)
   end
 end
