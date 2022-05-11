3b0e57b61510c605c29826804a60cfd6486139af
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-05-11 21:52:47 +0900

    Fix: rubocopの指摘修正

--- app/controllers/api/v1/user/diaries_controller.rb
+++ app/controllers/api/v1/user/diaries_controller.rb
@@ -1,19 +1,13 @@
 class Api::V1::User::DiariesController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def create
-  end
+  def create; end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
-  def update
-  end
+  def update; end
 
-  def destroy
-  end
+  def destroy; end
 end
