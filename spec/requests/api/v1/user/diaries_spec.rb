2b6708dfd65a367bd4cf802ff396314d0c32bbc4
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-05-12 19:37:53 +0900

    Fix: rspecのデフォルトテストを一時xit

--- spec/requests/api/v1/user/diaries_spec.rb
+++ spec/requests/api/v1/user/diaries_spec.rb
@@ -2,42 +2,42 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::User::Diaries", type: :request do
   describe "GET /index" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/api/v1/user/diaries/index"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /create" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/api/v1/user/diaries/create"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/api/v1/user/diaries/show"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /edit" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/api/v1/user/diaries/edit"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /update" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/api/v1/user/diaries/update"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /destroy" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/api/v1/user/diaries/destroy"
       expect(response).to have_http_status(:success)
     end
