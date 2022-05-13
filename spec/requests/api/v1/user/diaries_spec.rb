2c19d0ea7ef2975de813d896f7cdaab43de164a9
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-05-13 17:08:18 +0900

    Add: 推しメンの日記をを削除するrspec追加

--- spec/requests/api/v1/user/diaries_spec.rb
+++ spec/requests/api/v1/user/diaries_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe "推しメンの日記登録機能 Api::V1::User::Diaries", type:
     let(:http_request) { post api_v1_user_recommended_member_diaries_path(recommended_member.id), request_hash }
     context "正常系" do
       it "ユーザーが選択した推しメンの日記を作成できること" do
-        expect{ http_request }.to change { current_user.recommended_members.find_by(id: recommended_member.id).diaries.count }.by(1)
+        expect{ http_request }.to change { current_user.recommended_members.find_by(id: recommended_member.id).diaries.count }.by(-1)
         expect(response).to be_successful
         expect(response).to have_http_status(:ok)
       end
@@ -71,9 +71,14 @@ RSpec.describe "推しメンの日記登録機能 Api::V1::User::Diaries", type:
   end
 
   describe "ユーザーが選択した推しメンの日記を削除できること DELETE /api/v1/user/recommended_members/:recommended_member_id/diaries/:id" do
+    let!(:recommended_member) { create(:recommended_member, user: current_user) }
+    let!(:diary) { create(:diary, user: current_user, recommended_member: recommended_member) }
+    let(:http_request) { delete api_v1_user_recommended_member_diary_path(recommended_member, diary), headers: headers }
     context "正常系" do
-      it "ユーザーが選択した推しメンの日記を削除できること" do
-
+      fit "ユーザーが選択した推しメンの日記を削除できること" do
+        expect{ http_request }.to change { current_user.recommended_members.find_by(id: recommended_member.id).diaries.count }.by(-1)
+        expect(response).to be_successful
+        expect(response).to have_http_status(:ok)
       end
     end
   end
