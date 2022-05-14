1cd259025b943c86c94963ea2877b08cd5765e81
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-05-14 15:08:12 +0900

    Fix: ルーティングにformatjson追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :api do
+  namespace :api, format: 'json' do
     namespace :v1 do
       get :health_check, to: 'health_check#index'
       resources :users, only: [:create]
