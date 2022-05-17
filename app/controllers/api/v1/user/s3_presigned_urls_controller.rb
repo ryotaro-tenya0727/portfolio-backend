21e959262ba97dcf93c146238dbb46d01a0a2291
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-05-17 14:40:38 +0900

    Fix: S3署名URL取得時の記述のインデント揃える

--- app/controllers/api/v1/user/s3_presigned_urls_controller.rb
+++ app/controllers/api/v1/user/s3_presigned_urls_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::User::S3PresignedUrlsController < SecuredController
   def diary_presigned_url
-    authorize([:user, :s3_presigned_urls], :diary_presigned_url?)
+    authorize(%i[user s3_presigned_urls], :diary_presigned_url?)
     # diary_image = current_user.diary_images.build(diary_image_url: "#{ENV[CLOUDFRONT_DISTRIBUTION]}/#{diary_s3_url}")
     # diary_image.save!
     presigned_url = Signer.presigned_url(:get_object,
-                         bucket: ENV['S3_BUCKET'],
-                         key: diary_s3_url.to_s,
-                         expires_in: 300)
+                                         bucket: ENV['S3_BUCKET'],
+                                         key: diary_s3_url.to_s,
+                                         expires_in: 300)
     render json: presigned_url
   rescue ActiveRecord::RecordInvalid => e
     render400(e, recommended_member.errors.full_messages)
