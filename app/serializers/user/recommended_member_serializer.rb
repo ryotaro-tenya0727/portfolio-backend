d2ef3b8daf512e8e80a7913143ec017f1895b300
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-06-05 22:46:15 +0900

    Add: 推しメンとのチェキ数をシリアライザーに追加

--- app/serializers/user/recommended_member_serializer.rb
+++ app/serializers/user/recommended_member_serializer.rb
@@ -2,5 +2,12 @@ class User::RecommendedMemberSerializer
   include JSONAPI::Serializer
   attributes :id, :uuid, :nickname, :group, :first_met_date
 
+  attribute :total_member_polaroid_count do |object|
+    count = 0
+    object.diaries.each do |diary|
+      count += diary.event_polaroid_count || 0
+    end
+    count
+  end
   # チェキ総数、登録した日記、出会ってから経過した日数
 end
