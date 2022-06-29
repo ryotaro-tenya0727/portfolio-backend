d4487cd17b7eded5b09e886af2687ac4491c6721
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-06-29 20:28:55 +0900

    Fix: テストカバレッジの書き方修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,6 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'coveralls'
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -91,5 +90,4 @@ RSpec.configure do |config|
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
 
-Coveralls.wear!
 end
