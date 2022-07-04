e6d2895f1b91dbaea7777273ec8e01b189c6db2d
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-07-04 17:15:30 +0900

    Fix: CORS修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'https://www.oshi-diary.com/'
+    origins 'http://localhost:3001', 'https://www.oshi-diary.com'
 
     resource '*',
       headers: :any,
