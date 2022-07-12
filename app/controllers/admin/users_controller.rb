b5638bf1583485f1db7d1c786d5e8ca231d16fe4
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-07-12 09:35:57 +0900

    ADD 管理ユーザーのN+1解消

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < SecuredController
   def index
-    @users = policy_scope(User, policy_scope_class: Admin::UserPolicy::Scope)
+    @users = policy_scope(User, policy_scope_class: Admin::UserPolicy::Scope).preload(:diaries)
     render_json = Admin::AdminUserSerializer.new(@users)
     render json: render_json, status: :ok
   end
