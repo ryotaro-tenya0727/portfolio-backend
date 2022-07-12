7d844ea00b9593690fd1cc17891e1ef14f07049b
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-07-12 09:51:54 +0900

    ADD: 管理ユーザー機能のポリシー変更

--- app/policies/admin/user_policy.rb
+++ app/policies/admin/user_policy.rb
@@ -14,7 +14,7 @@ class Admin::UserPolicy < ApplicationPolicy
       if user.admin?
         scope.all
       else
-        return
+        raise Pundit::NotAuthorizedError
       end
     end
   end
