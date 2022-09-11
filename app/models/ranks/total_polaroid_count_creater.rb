b8249d24df83417d7d29eeb9cc109cf812883b06
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-09-11 10:52:41 +0900

    ADD: チェキ数ランキングを作成するModel追加

--- app/models/ranks/total_polaroid_count_creater.rb
+++ app/models/ranks/total_polaroid_count_creater.rb
@@ -1,7 +1,7 @@
 class Ranks::TotalPolaroidCountCreater
   def self.create_ranking
     User.all
-      .preload(:followers, :recommended_members, :diaries)
-      .sort_by { |user| user.diaries.pluck(:event_polaroid_count).sum * -1 }
+        .preload(:followers, :recommended_members, :diaries)
+        .sort_by { |user| user.diaries.pluck(:event_polaroid_count).sum * -1 }
   end
 end
