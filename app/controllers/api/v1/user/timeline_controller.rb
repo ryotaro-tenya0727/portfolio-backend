3d3e0acc8d6d67a9ed12ee3378afc754befb9a51
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-09-13 15:18:32 +0900

    ADD: Kaminariでwithout_countの使い方確認

--- app/controllers/api/v1/user/timeline_controller.rb
+++ app/controllers/api/v1/user/timeline_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::User::TimelineController < SecuredController
   def index
     time_line = if current_user.nil?
-                  Diary.all.preload(:diary_images, :recommended_member, :user).order(created_at: :desc)
+                  Diary.all
+                       .page(1)
+                       .without_count
+                       .preload(:diary_images, :recommended_member, :user)
+                       .order(created_at: :desc)
                 else
                   current_user.time_line
                 end
