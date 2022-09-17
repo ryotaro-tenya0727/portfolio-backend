5e0eba0251cac034a23a8e8eab68f8eab3eb6c55
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-09-17 19:40:52 +0900

    Fix: タイムラインコントローラーの空行修正

--- app/controllers/api/v1/user/timeline_controller.rb
+++ app/controllers/api/v1/user/timeline_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::User::TimelineController < SecuredController
   def index
-    time_line =  Diary.all
-                      .page(params[:page])
-                      .without_count
-                      .preload(:diary_images, :recommended_member, :user, :like_users)
-                      .order(created_at: :desc)
+    time_line = Diary.all
+                     .page(params[:page])
+                     .without_count
+                     .preload(:diary_images, :recommended_member, :user, :like_users)
+                     .order(created_at: :desc)
     render_json = User::TimelineSerializer.new(time_line, current_user: current_user).serializable_hash.to_json
     render json: render_json, status: :ok
   end
