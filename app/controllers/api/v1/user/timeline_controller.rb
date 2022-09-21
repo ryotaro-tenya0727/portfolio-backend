22b9ab96a1bcd62220007833876d82100f46d9af
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-09-21 13:02:36 +0900

    Change: タイムラインのメソッドチェーンの順番変更

--- app/controllers/api/v1/user/timeline_controller.rb
+++ app/controllers/api/v1/user/timeline_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::User::TimelineController < SecuredController
   def index
     time_line = Diary.all
+                     .order(created_at: :desc)
                      .page(params[:page])
                      .without_count
                      .preload(:diary_images, :recommended_member, :user, :like_users)
-                     .order(created_at: :desc)
+
     render_json = User::TimelineSerializer.new(time_line, current_user: current_user).serializable_hash.to_json
     render json: render_json, status: :ok
   end
