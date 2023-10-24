cd41836c0fe2870cf3aeac9f16f8a98d8b737383
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2023-10-24 23:24:11 +0900

    環境変数変更

--- app/controllers/api/v1/user/s3_presigned_urls_controller.rb
+++ app/controllers/api/v1/user/s3_presigned_urls_controller.rb
@@ -2,10 +2,10 @@ class Api::V1::User::S3PresignedUrlsController < SecuredController
   def diary_presigned_url
     authorize(%i[user s3_presigned_urls], :diary_presigned_url?)
     presigned_url = Signer.presigned_url(:put_object,
-                                         bucket: ENV['S3_BUCKET'],
+                                         bucket: ENV['IMAGE_S3_BUCKET'],
                                          key: diary_s3_url.to_s,
                                          expires_in: 3600)
-    render json: { presigned_url: presigned_url, diary_image_url: "#{ENV['CLOUDFRONT_DISTRIBUTION']}/#{diary_s3_url}" }
+    render json: { presigned_url: presigned_url, diary_image_url: "#{ENV['IMAGE_CLOUDFRONT_DISTRIBUTION']}/#{diary_s3_url}" }
   end
 
   private
@@ -15,6 +15,6 @@ class Api::V1::User::S3PresignedUrlsController < SecuredController
   end
 
   def diary_s3_url
-    "#{ENV['S3_DIARY_OBJECT_KEY']}/#{current_user.id}/diary/#{presigned_url_params[:filename]}"
+    "#{ENV['IMAGE_S3_DIARY_OBJECT_KEY']}/#{current_user.id}/diary/#{presigned_url_params[:filename]}"
   end
 end
