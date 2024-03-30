72778662619c6b5390f02316177de585731f07a7
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2024-03-30 23:17:13 +0900

    ADD: spec追加

--- spec/requests/api/v1/user/external/aws/s3/profiles_spec.rb
+++ spec/requests/api/v1/user/external/aws/s3/profiles_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "署名URLの取得 Api::V1::User::S3PresignedUrls", type: :request do
+RSpec.describe "署名URLの取得 Api::V1::User::External::Aws::S3::Presigned_url::Profile", type: :request do
   let!(:current_user) { create(:user) }
   let(:headers) { { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json', Authorization: 'jwt_test_token' } }
 
@@ -8,9 +8,9 @@ RSpec.describe "署名URLの取得 Api::V1::User::S3PresignedUrls", type: :reque
     authorization_stub
   end
 
-  describe 'ユーザーがS3へアップロードするための署名URLを取得 GET POST /api/v1/user/s3_presigned_url' do
+  describe 'ユーザーがS3へアップロードするための署名URLを取得 POST /api/v1/user/external/aws/s3/presigned_url/profiles' do
     let!(:request_hash) { { headers: headers, params: { presigned_url: {filename: "test"} }.to_json } }
-    let(:http_request) { post '/api/v1/user/s3_presigned_url', request_hash }
+    let(:http_request) { post '/api/v1/user/external/aws/s3/presigned_url/profiles', request_hash }
 
     before do
       allow(Signer).to receive(:presigned_url).and_return("presigned_url")
